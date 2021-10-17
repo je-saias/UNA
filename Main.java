b654ca875a555c7cfdf1082d9f5ae71e4e5368d1
Jesaias Simao <90730732+je-saias@users.noreply.github.com>
2021-10-17 18:30:20 +0000

    Java

--- Main.java
+++ Main.java
@@ -1,44 +1,49 @@
 import java.util.Scanner;
 
-class Main {
- public static void main(String[] args) {
+class Main { 
+  public static void main(String[] args) {
 
-int idade = 0;
-int idades = 0;
-int soma = 0;
-double media = 0.0;
 
-Scanner leitor = new Scanner(System.in);
+    int idade = 0;
+    int idades = 0;
+    int soma = 0;
+    double media = 0.0;
 
-  System.out.println("Digite a idades");
-  idade = leitor.nextInt();
+    Scanner leitor = new Scanner(System.in);
 
-  do {  
-       
-
-  if (idade>0) { 
-    System.out.println("Digite a idade");
+    System.out.println("Digite a idades");
     idade = leitor.nextInt();
 
-      soma = soma + idade;
-      idades++;
-  
-  }
-  else{}
+    do {
+       
+       
 
-  } 
+      if (idade>0) {
+
+        System.out.println("Digite a idade");
+        idade = leitor.nextInt();
+
+        soma = soma + idade;
+        idades++;
   
-  while (idade>0);
+      }
+
+      else{}
+
+
+    } 
+  
+    while (idade>0);
 
     media = soma / idades;
 
     System.out.println("A média das idades é " +media);
 
-	leitor.close();
+	  leitor.close();
 	
-   }
-
   }
+
+}
 
 
 
